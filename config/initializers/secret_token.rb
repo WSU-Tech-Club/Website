41bc9f0674f5759108b70328724948a70794cb2d
Dylan Powers <dylan.powers@email.wsu.edu>
2012-06-21 17:05:11 -0700

    First commit: New rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TechClubWebsite::Application.config.secret_token = '5a33242b9a19ebdf6f927409be372b5b8b3b2d2b57b8c8b49bed75268a37891a1901d41a3ba5a46e97d2d0ff8293227456277d5e4ac5919a8267362fdd5a14e6'
